d94ab7a79b9465e0fd001f9c0e34df40663c6ada
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-09 22:33:00 -0800

    Add getNewSize and update createGrid

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ createGrid(16);
 function createGrid(selectedSize) {
 	const gridSize = selectedSize ** 2;
 
-	const containerDiv = document.createElement("div");
-	containerDiv.classList.add("container");
-	containerDiv.addEventListener("mouseover", changeBackground);
+	const main = document.querySelector(".main");
+	const currentContainer = document.querySelector(".container");
+	const newContainer = document.createElement("div");
+	newContainer.classList.add("container");
+	newContainer.addEventListener("mouseover", changeBackground);
 
 	for (let i = 1; i <= gridSize; i++) {
 		const itemDiv = document.createElement("div");
 		itemDiv.classList.add("item");
 
-		containerDiv.appendChild(itemDiv);
+		newContainer.appendChild(itemDiv);
 	}
 
-	const main = document.querySelector(".main");
-	main.appendChild(containerDiv);
+	if (currentContainer) {
+		newContainer.style.gridTemplateColumns = `repeat(${selectedSize}, 1fr)`;
+		currentContainer.replaceWith(newContainer);
+	} else {
+		main.appendChild(newContainer);
+	}
 }
 
 function changeBackground(e) {
@@ -31,4 +37,13 @@ const resizeDialog = document.querySelector(".resize-dialog");
 
 function showResizeDialog() {
 	resizeDialog.showModal();
+}
+
+const resizeForm = document.querySelector(".resize-form");
+const resizeInput = document.querySelector("#new-size");
+resizeForm.addEventListener("submit", getNewSize);
+
+function getNewSize() {
+	const newSize = document.querySelector("#new-size");
+	createGrid(+newSize.value);
 }
