307e9e37a9047baff4f64278204036dba36eb157
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-09 17:43:31 -0800

    Add createGrid function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello there");
+"use strict";
+
+createGrid(16);
+
+function createGrid(selectedSize) {
+	const gridSize = selectedSize ** 2;
+
+	const containerDiv = document.createElement("div");
+	containerDiv.classList.add("container");
+
+	for (let i = 1; i <= gridSize; i++) {
+		const itemDiv = document.createElement("div");
+		itemDiv.classList.add("item");
+
+		containerDiv.appendChild(itemDiv);
+	}
+
+	const body = document.querySelector("body");
+	body.appendChild(containerDiv);
+}
